094aa960c4ad1fe77d91e5a92611df4159f7cb79
Guillaume <pascailguillaume@gmail.com>
2023-10-13 14:12:09 +0200

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module personne.personne {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens personne.personne to javafx.fxml;
+    exports personne.personne;
+}
