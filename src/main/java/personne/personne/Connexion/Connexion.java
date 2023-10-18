6be956cb9ab779231d5c3e1197192eeb5f3df47f
Guillaume <pascailguillaume@gmail.com>
2023-10-18 09:10:22 +0200

    Ajout connexion à la BDD

---
+++ src/main/java/personne/personne/Connexion/Connexion.java
@@ -0,0 +1,31 @@
+package personne.personne.Connexion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Connexion {
+    private String jdbcUrl;
+    private String username;
+    private String password;
+    private Connection connection;
+
+    public Connexion() {
+
+    }
+
+    public void connect() throws SQLException {
+        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/personne", "root", "");
+    }
+
+    public void close() throws SQLException {
+        if (connection != null && !connection.isClosed()) {
+            connection.close();
+        }
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+//public class Connexion {
+}
